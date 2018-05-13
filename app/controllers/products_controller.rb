96e32deb9ede8ddbc245fe4eef127c0031c1c63b
Marek <yurchylyak@gmail.com>
2018-05-13 14:11:45 +0000

    edit button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,25 +1,53 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  
   def index
     @products = Product.all
+  end
+  
+  def show
   end
 
   def new
     @product = Product.new
   end
+  
+  def edit
+  end
 
   def create
     @product = Product.new(product_params)
 
-    if @product.valid?
-      @product.save
-
-      redirect_to products_path
-    else
-      render :new
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to products_path, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  # PATCH/PUT /products/1
+  # PATCH/PUT /products/1.json
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   private
+  
+  def set_product
+      @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:title, :price)
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_11_161252) do
+ActiveRecord::Schema.define(version: 2018_05_13_133808) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
