031a1b6272d46e6a93c98a0547bea6127765c8ca
Igor Frynza <frunza@2029gmail.com>
2018-05-14 21:13:29 +0300

    added authentification

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :products
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'log_in' => 'sessions#new'
+  delete 'log_out' => 'sessions#destroy'
+  get 'sign_up' => 'users#new'
 
+  resources :products
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  
   root 'products#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_13_133808) do
+ActiveRecord::Schema.define(version: 2018_05_14_163243) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_05_13_133808) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
